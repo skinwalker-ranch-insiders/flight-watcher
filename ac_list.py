7587988206fd37898dfadebb55134080b129f39c
RK Davies <rkdavies@hackshack.sh>
2023-03-02 14:46:28 -0800

    Moving AC hotlist Alert

--- ac_list.py
+++ ac_list.py
@@ -1,1 +1,5 @@
-acHotList = ['List'], ['Of'], ['AC'], ['Goes'], ['Here']
+""" Get Alerts for AC of Interest """
+
+hot_alert_url = 'https://www.youtube.com/watch?v=-bzWSJG93P8' 
+
+acHotList = ['LIST', 'OF-TAIL', 'NUMBERS']
